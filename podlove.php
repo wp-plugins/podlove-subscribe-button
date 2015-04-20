9cf0b4e331f112e1fc16e56a7857fbfc00f6cf7f
chemiker <chemiker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 19:40:19 +0000

    Version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/podlove-subscribe-button/tags/1.1@1139561 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ podlove.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Plugin Name: Podlove Subscribe Button
+ * Plugin URI:  http://wordpress.org/extend/plugins/podlove-subscribe-button/
+ * Description: Brings the Podlove Subscribe Button to your WordPress installation.
+ * Version:     1.1
+ * Author:      Podlove
+ * Author URI:  http://podlove.org
+ * License:     MIT
+ * License URI: license.txt
+ * Text Domain: podlove
+ */
+
+require('settings/buttons.php');
+// Models
+require('model/base.php');
+require('model/button.php');
+require('model/network_button.php');
+// Table
+require('settings/buttons_list_table.php');
+// Media Types
+require('media_types.php');
+// Widget
+require('widget.php');
+
+add_action( 'admin_menu', array( 'PodloveSubscribeButton', 'admin_menu') );
+if ( is_multisite() )
+	add_action( 'network_admin_menu', array( 'PodloveSubscribeButton', 'admin_network_menu') );
+
+add_action( 'admin_init', array( 'PodloveSubscribeButton', 'register_settings') );
+add_action( 'admin_init', array( 'PodloveSubscribeButton\Settings\Buttons', 'process_form' ) );
+
+add_action( 'admin_enqueue_scripts', function () {
+	wp_register_style( 'podlove-subscribe-button', plugin_dir_url(__FILE__).'style.css' );
+	wp_enqueue_style( 'podlove-subscribe-button' );
+} );
+
+// Register Settings
+add_action( 'admin_init', function () {
+	register_setting( 'podlove-subscribe-button', 'podlove_subscribe_button_default_style' );
+	register_setting( 'podlove-subscribe-button', 'podlove_subscribe_button_default_autowidth' );
+} );
+
+add_shortcode( 'podlove-subscribe-button', array( 'PodloveSubscribeButton', 'shortcode' ) );
+
+class PodloveSubscribeButton {
+
+	public static function admin_menu() {
+		add_options_page(
+				'Podlove Subscribe Button Options',
+				'Podlove Subscribe Button',
+				'manage_options',
+				'podlove-subscribe-button',
+				array( 'PodloveSubscribeButton\Settings\Buttons', 'page')
+			);
+	}
+
+	public static function admin_network_menu() {
+		add_submenu_page(
+				'settings.php',
+				'Podlove Subscribe Button Options',
+				'Podlove Subscribe Button',
+				'manage_options',
+				'podlove-subscribe-button',
+				array( 'PodloveSubscribeButton\Settings\Buttons', 'page')
+			);
+	}
+
+	public static function register_settings() {
+		\PodloveSubscribeButton\Model\Button::build();
+		if ( is_multisite() )
+			\PodloveSubscribeButton\Model\NetworkButton::build();
+	}
+
+	public static function shortcode( $args ) {
+		if ( ! $args || ! isset($args['button']) )
+			return __('You need to create a Button first and provide its ID.', 'podlove');
+
+		if ( ! $button = ( \PodloveSubscribeButton\Model\Button::find_one_by_property('name', $args['button']) ? \PodloveSubscribeButton\Model\Button::find_one_by_property('name', $args['button']) : \PodloveSubscribeButton\Model\NetworkButton::find_one_by_property('name', $args['button']) ) )
+			return sprintf( __('Oops. There is no button with the ID "%s".', 'podlove'), $args['button'] );
+
+		if ( isset($args['width']) && $args['width'] == 'auto' ) {
+			$autowidth = 'on';
+		} else {
+			$autowidth = get_option('podlove_subscribe_button_default_autowidth', 'on');
+		}
+
+		if ( isset($args['size']) && in_array($args['size'], array('small', 'medium', 'big', 'big-logo')) ) {
+			$size = $args['size'];
+		} else {
+			$size = get_option('podlove_subscribe_button_default_style', 'big-logo');
+		}
+
+		return $button->render( $size, $autowidth );
+	}
+
+}
---
+++ widget.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace PodloveSubscribeButton;
+
+class Podlove_Subscribe_Button_Widget extends \WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+					'podlove_subscribe_button_widget',
+					'Podlove Subscribe Button',
+					array( 'description' => __( 'Adds a Podlove Subscribe Button to your Sidebar', 'podlove' ), )
+				);
+	}
+
+	public function widget( $args, $instance ) {
+		$button = ( \PodloveSubscribeButton\Model\Button::find_by_id($instance['button']) ? \PodloveSubscribeButton\Model\Button::find_by_id($instance['button']) : \PodloveSubscribeButton\Model\NetworkButton::find_by_id($instance['button']) );
+
+		echo $args['before_widget'];
+		echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+
+		echo $button->render($instance['style'], $instance['autowidth']);
+		
+		if ( strlen($instance['infotext']) )
+			echo wpautop($instance['infotext']);
+
+		echo $args['after_widget'];
+	}	
+
+	public function form( $instance ) {
+		$title     = isset( $instance[ 'title' ] )     ? $instance[ 'title' ]      : '';
+		$button    = isset( $instance[ 'button' ] )    ? $instance[ 'button' ]     : '';
+		$style     = isset( $instance[ 'style' ] )     ? $instance[ 'style' ]      : '';
+		$autowidth = isset( $instance[ 'autowidth' ] ) ? $instance[ 'autowidth' ]  : 0;
+		$infotext  = isset( $instance[ 'infotext' ] )  ? $instance[ 'infotext' ]   : '';
+		$buttons = \PodloveSubscribeButton\Model\Button::all();
+		$network_buttons = \PodloveSubscribeButton\Model\NetworkButton::all();
+
+		$buttons_as_options = function ($buttons) {
+			foreach ($buttons as $subscribebutton) {
+				echo "<option value='".$subscribebutton->id."' ".( $subscribebutton->id == $button ? 'selected=\"selected\"' : '' )." >".$subscribebutton->title." (".$subscribebutton->name.")</option>";
+			}
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'podlove' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $title; ?>" />
+
+			<label for="<?php echo $this->get_field_id( 'button' ); ?>"><?php _e( 'Button', 'podlove' ); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'button' ); ?>"
+				      name="<?php echo $this->get_field_name( 'button' ); ?>">
+				<?php if ( isset($network_buttons) && count($network_buttons) > 0 ) : ?>
+					<optgroup label="<?php _e('Local', 'podlove'); ?>">
+						<?php $buttons_as_options($buttons); ?>
+					</optgroup>
+					<optgroup label="<?php _e('Network', 'podlove'); ?>">
+						<?php $buttons_as_options($network_buttons); ?>
+					</optgroup>
+				<?php else : 
+					$buttons_as_options($buttons);
+				 endif; ?>
+			</select>
+
+			<label for="<?php echo $this->get_field_id( 'style' ); ?>"><?php _e( 'Style', 'podlove' ); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'style' ); ?>" name="<?php echo $this->get_field_name( 'style' ); ?>">
+				<option value="default"      <?php echo ( $style == 'default'    ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Default Style', 'podlove' ) ?></option>
+				<optgroup>
+					<option value="small"    <?php echo ( $style == 'small'    ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Small', 'podlove' ) ?></option>
+					<option value="medium"   <?php echo ( $style == 'medium'   ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'medium', 'podlove' ) ?></option>
+					<option value="big"      <?php echo ( $style == 'big'      ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Big', 'podlove' ) ?></option>
+					<option value="big-logo" <?php echo ( $style == 'big-logo' ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Big with logo', 'podlove' ) ?></option>
+				</optgroup>
+			</select>
+
+			<label for="<?php echo $this->get_field_id( 'autowidth' ); ?>"><?php _e( 'Autowidth', 'podlove' ); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'autowidth' ); ?>" name="<?php echo $this->get_field_name( 'autowidth' ); ?>">
+				<option value="default"      <?php echo ( $autowidth == 'default'    ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Default Autowidth', 'podlove' ) ?></option>
+				<optgroup>
+					<option value="on"       <?php echo ( $autowidth == 'on'    ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'Yes', 'podlove' ) ?></option>
+					<option value=""         <?php echo ( $autowidth == ''   ? 'selected=\"selected\"' : '' ); ?>><?php _e( 'No', 'podlove' ) ?></option>
+				</optgroup>
+			</select>
+		
+			<label for="<?php echo $this->get_field_id( 'infotext' ); ?>"><?php _e( 'Description', 'podlove' ); ?></label> 
+			<textarea class="widefat" rows="10" id="<?php echo $this->get_field_id( 'infotext' ); ?>" name="<?php echo $this->get_field_name( 'infotext' ); ?>"><?php echo $infotext; ?></textarea>
+		</p>
+		<?php 
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['infotext']  = ( ! empty( $new_instance['infotext'] ) )  ? $new_instance['infotext']                : '';
+		$instance['title']     = ( ! empty( $new_instance['title'] ) )     ? strip_tags( $new_instance['title'] )     : '';
+		$instance['style']     = ( ! empty( $new_instance['style'] ) )     ? strip_tags( $new_instance['style'] )     : '';
+		$instance['autowidth'] = ( ! empty( $new_instance['autowidth'] ) ) ? strip_tags( $new_instance['autowidth'] ) : 0;
+		$instance['button']    = ( ! empty( $new_instance['button'] ) )    ? strip_tags( $new_instance['button'] )    : '';
+		return $instance;
+	}
+}
+add_action( 'widgets_init', function(){
+     register_widget( '\PodloveSubscribeButton\Podlove_Subscribe_Button_Widget' );
+});
